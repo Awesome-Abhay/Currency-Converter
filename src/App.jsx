6ba3bb9b29bdff562e5585b638c768ee8bcaba98
Abhay Agrawal <awesome.abhay20@gmail.com>
2025-02-13 07:45:21 +0530

    feat: Currency Converter initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,54 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 import OneBox from './components/OneBox';
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [amount, setAmount] = useState(0);
+  let [from, setFrom]= useState("USD");
+  let [to, setTo]= useState("INR");
+  let [convertedAmount, setConvertedAmount]= useState(0);
+
+
+  let swap= ()=>{
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  }
 
   return (
     <>
       <h1 className="heading p-5 text-white text-4xl text-center mt-20">Currency Converter</h1>
       <div className="main max-w-md rounded-xl shadow-sm shadow-white p-5 border-2 border-white mx-auto mt-10 flex flex-col gap-4 relative">
+
+        <OneBox
+          label={from}
+          amountDisable={false}
+          amount={amount}
+          onAmountChange={(amount)=> setAmount(amount)}
+          selectCurrency={from}
+          currencyDisable={false}
+
+        />
         
-      <OneBox />
-      <OneBox />
+        <OneBox 
+          label={to}
+          amountDisable={true}
+          selectCurrency={to}
+          currencyDisable={true}
+          amount={convertedAmount}
+          
+        />
 
-      <button className="swap w-15 bg-blue-700 text-white text-xl flex justify-center items-center h-10 shadow-sm rounded-sm absolute top-43/100 left-40/100 border-1 border-white cursor-pointer">swap</button>
+        <button 
+          className="swap w-15 bg-blue-700 text-white text-xl flex justify-center items-center h-10 shadow-sm rounded-sm absolute top-35/100 left-40/100 border-1 border-white cursor-pointer"
+          onClick={swap}
+          >swap</button>
 
+        <button className="convert w-full bg-blue-500 text-white rounded-xl p-2 flex justify-center items-center text-2xl cursor-pointer">Convert From {from} To {to}</button>
 
-
-        
       </div>
     </>
   )
--- src/components/OneBox.jsx
+++ src/components/OneBox.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React,{useId} from 'react'
 
-function OneBox() {
+function OneBox({
+    label,
+    amountDisable,
+    amount,
+    onAmountChange,
+    selectCurrency,
+    currencyDisable,
+}) {
+
+    const amountInputId= useId();
+
     return (
         <div className='one-box rounded-xl w-full border-2 bg-white p-5 flex flex-col gap-5'>
             <div className='flex w-full'>
-                <div className='w-1/2'>From</div>
+                <label htmlFor={amountInputId} className='w-1/2'>{label}</label>
                 <div className='w-1/2 text-end'>Currency Type</div>
             </div>
             <div className='flex justify-between'>
                 <input
+                    id={amountInputId}
+                    placeholder='Amount'
                     type="number"
-                    defaultValue={0}
-                    className='w-1/2 outline-none' />
-                <select className='outline-none w-1/5 bg-slate-100'>
+                    value={amount}
+                    className='w-1/2 outline-none'
+                    disabled={amountDisable}
+                    onChange={(e)=>  onAmountChange(e.target.value)}
+                />
+                <select 
+                    className='outline-none w-1/5 bg-slate-100'
+                    value={selectCurrency}  
+                    disabled={currencyDisable}  
+                >
                     <option value="USD">US</option>
                     <option value="EUR">EUR</option>
                     <option value="GBP">GB</option>
